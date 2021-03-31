d6df448a96868ee3ac88a13e02ede94a8453519a
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-03-31 13:01:38 +0100

    Update files

--- script.js
+++ script.js
@@ -17,23 +17,43 @@
 
         // EX12) Write a function to change the color of the main title
 
+        const changeColorTitle = function(){
+
+                let title = document.querySelector('.jumbotron > div > h1')
+                    title.style.color = "firebrick"
+        }
+            // changeColorTitle()
 
 
         // EX13) Write a function to change the background of the jumbotron
 
-        // const bgColorJumbotron = function(){
-        //    let jumbotron = document.querySelector('div[class*="jumbotron"]')
-        //    jumbotron.style.backgroundColor = 'red !important'
-        // }
-        //     bgColorJumbotron()
-
-      
-
-        
+        const bgColorJumbotron = function(){
+           let jumbotron = document.querySelector('div[class*="jumbotron"]')
+           jumbotron.classList.remove('bg-dark')
+           jumbotron.style.backgroundColor = 'lightgreen'
+        }
+            // bgColorJumbotron()
 
         // EX14) Write a function to remove all the links under "Elsewhere"
 
+        const removeLinks = () =>{
+
+            // let list = document.querySelector('aside > div[class*=\'p-4\']:nth-child(3) > ol')
+            // let links =  list.querySelectorAll('a')
+            let list = document.querySelectorAll('.list-unstyled')[1]
+            // links.forEach( link => link.remove())
+            list.remove()
+            console.log(list)
+        }
+        //  removeLinks()
+
         // EX15) Write a function to change the column size for post headings
+
+        const headingColChange = function(){
+            let blog = document.querySelectorAll('.col-md-8 blog-main > h2:first-of-type')
+            console.log(blog)
+        }
+            headingColChange()
 
         // EX16) Write a function to remove the "Search" magnifying glass icon
 
